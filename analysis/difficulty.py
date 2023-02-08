2c74d8da3e791b526beaa835ac6fbc0ed8f15600
James Santucci <james.santucci@gmail.com>
2023-02-07 21:44:02 -0700

    make mypy work kinda

--- analysis/difficulty.py
+++ analysis/difficulty.py
@@ -1,7 +1,6 @@
 from typing import Optional
 import matplotlib.pyplot as plt  # type: ignore
 import pandas as pd  # type: ignore
-import seaborn as sns  # type: ignore
 
 from filters import get_finalist_climbs, get_finalist_tops, get_finalist_zones
 from plotting import stripplot_by_round
@@ -43,7 +42,7 @@ def year_difficulty_tops_category(df: pd.DataFrame) -> None:
     plt.clf()
 
 
-def alt_difficulty(df: pd.DataFrame, hue: str = None) -> None:
+def alt_difficulty(df: pd.DataFrame, hue: Optional[str] = None) -> None:
     _, axes = plt.subplots(1, 3, figsize=(8, 3))
     axes[1].set_xlim(0.8, 4)
     axes[2].set_xlim(axes[1].get_xlim())
